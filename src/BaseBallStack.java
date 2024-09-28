88dbc15ff7fef46c475f8244e5420eb1cfb145f3
Norumai01 <johnnydinhnguyen2001@gmail.com>
2024-09-28 01:41:55 -0400

    Finished baseball leetcode

--- src/BaseBallStack.java
+++ src/BaseBallStack.java
@@ -7,16 +7,17 @@ public class BaseBallStack {
         for (String operation : operations) {
             switch (operation) {
                 case "+" -> {
-                    stack.push(stack.pop() + stack.pop());
+                    int valueOne = stack.get(stack.size() - 2);
+                    int valueTwo = stack.peek();
+                    stack.push(valueOne + valueTwo);
                 }
-                case "C" -> {
-                    stack.push(stack.pop() * stack.pop());
-                }
-                case "D":
-                    break;
+                case "C" -> stack.pop();
+                case "D" -> stack.push(stack.peek() * 2);
+                default -> stack.push(Integer.parseInt(operation));
             }
         }
-        return -1;
+        if (stack.isEmpty()) return 0;
+        return stack.stream().mapToInt(x -> x).sum();
     }
 
     public static void main(String[] args) {
